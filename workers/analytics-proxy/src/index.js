1615ff2e84afb75c7ff0ab755fa7f9fdd2d01587
alkait <alkait@gmail.com>
2026-03-18 02:28:32 +0400

    Security: remove service account credentials from source code, use env vars instead

--- workers/analytics-proxy/src/index.js
+++ workers/analytics-proxy/src/index.js
@@ -1,39 +1,5 @@
-const GA4_PROPERTY_ID = '528971922';
-const GA4_API_URL = `https://analyticsdata.googleapis.com/v1beta/properties/${GA4_PROPERTY_ID}:runReport`;
 const CACHE_TTL = 86400;
-
-const SERVICE_ACCOUNT = {
-  client_email: 'ebrahimalzaabi-com@ebrahimalzaabi-com.iam.gserviceaccount.com',
-  private_key: `-----BEGIN PRIVATE KEY-----
-MIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCzKKoZhreChDQf
-g+esOA119vL0QLUtw8thZ5sKBHWJh22PqCe2UMPB1QNXPQExNzd+brPdzydT13Ok
-kBYnDJkS6ewS+z9cu7QenmDN8Rtw5myMG/l5Dlx02RV4uXmLs1+wU450nkghOLAZ
-J28+vktfeeSEu2/3xWW0NtCVGE4ojP3ZF1Pa2aADKFyvlCQUnXuz24n76DsPmAYC
-GQa6/JpJeT4T3T4yCwYmcPZAPNXOaGWxGrMwygNLc0WCnuiMgPflUg3aYNJKlNFz
-dEfrwMOj1nFBi8WjoliSVS3thaclu8ERVxBEOpu/0H0vON0Z+jPwsfCfF2td1jMn
-HD2sp+3JAgMBAAECggEADk9AqtJGRP/vM9cywWAmvxXO2PB0KCgbzfda/EVX5/iv
-qQo6xcaUpB4x6p1eQ4KQBaKs7hn2H621OF8jzdGlRkv1xZxYtrztSYbH35sS4IUf
-BV/UoLUjcFfIbcCOszhL+8bouWrNAw5GjnPGYGINnh4bwPhBnK4hDfBU35u13mom
-QulmhlE34zeI7xJ4fB3byHbDTLTnd1Ba/5qS/Msj1YbD5n7CkGGVjOjDXHff5THP
-LDVUt5mKm73OXOb5oWUdA5zvHjW5Oaz121ibX9okMdi+f+gsKiN/vcMc+9co0JkJ
-faP4e5FXIICVQSroNyUcRCW/IrZgDPwH94Ve62VaKwKBgQDaF1GeUUn6b7JTR2uC
-NRb6G3wnVmEGQh8bIS84owjgsVu9WHCPKNqo7hUgp09eAorhMdWXhWkt3b8dt2Rw
-GUx66VJ7JPRKxdAbWmQRletvxkFW1z9oZI98FvKRaAvFW1SNB9PozKUW3oQWGLkr
-cd0ZcHv7fdF0nJiK1Ald+bME2wKBgQDSTO1gEB1qhluk+PmU6KEwhGqlNaUg4xj/
-Ob9/JAzVBcIgAdd+ABwMAvZQ7X/gKLQyv9m86rlFQ22tJrDrgiiPPWN6MxiuxVcP
-03JuBGEM87cshxI8UTW8wONeoftBiV1xL5A0xIv5CkvNodMux+lKqTGRC5U0I5yi
-y6j+noZnKwKBgDTuZ+7Gx++yYFN6CAvIGtWFCjL58f502dvZx/41A5iVMzDXYCo+
-viM19YbBVK/6af6R+8cUqBxrr1DlC8lxOnCyZERjCa+EJygcXiEug7/THFSZVVhe
-37w8TY8g84poyjKYbcQYtxC/3MsIIRj8P69G77BwLv/gad137PYT/nD1AoGALKrq
-QF0r4lRnYsL/QAGpOwTlIfIjALk7tfouEnM1xolSkjnR65FaY7K9XrMTVilPHPhi
-5x1z/KI1yXFOYrdipgoD9xCjTXvc4h+nTnbfpkCdx3dpCCa3oz/VC2ScWW8zybaw
-ioS0tlDZiPO2R1lu08zcGlK7fjeSLYHUg0Boi8ECgYAtJ4+9ENLvz7xAvmGuJ99Y
-ytoJ1KeJRkcSC0o6c4g0LOTjDABiHZSQ9L+IPWRtkXuu0RPgIB0wxjMt1eFp9zbd
-SD0JtEjhg3QtthJSqNsgwk9nD2T7XlUVRIw0ysF6rA1yvfltvPskQu5nxoqddGtd
-KQgDXx45teVou/E5bZma7Q==
------END PRIVATE KEY-----`,
-  token_uri: 'https://oauth2.googleapis.com/token'
-};
+const TOKEN_URI = 'https://oauth2.googleapis.com/token';
 
 export default {
   async fetch(request, env, ctx) {
@@ -44,14 +10,14 @@ export default {
     }
 
     if (url.pathname === '/analytics' && request.method === 'GET') {
-      return handleAnalytics(url, ctx);
+      return handleAnalytics(url, ctx, env);
     }
 
     return new Response('Not Found', { status: 404 });
   }
 };
 
-async function handleAnalytics(url, ctx) {
+async function handleAnalytics(url, ctx, env) {
   try {
     const period = url.searchParams.get('period') || '7d';
     const { startDate, endDate, label } = parsePeriod(period);
@@ -72,9 +38,10 @@ async function handleAnalytics(url, ctx) {
       return jsonResponse({ ...cachedData, cached: true });
     }
 
-    const accessToken = await getAccessToken();
+    const accessToken = await getAccessToken(env);
+    const apiUrl = `https://analyticsdata.googleapis.com/v1beta/properties/${env.GA4_PROPERTY_ID}:runReport`;
 
-    const gaResponse = await fetch(GA4_API_URL, {
+    const gaResponse = await fetch(apiUrl, {
       method: 'POST',
       headers: {
         'Authorization': `Bearer ${accessToken}`,
@@ -182,9 +149,9 @@ function parseGA4Response(data, label, startDate, endDate) {
 
 // --- GA4 JWT Auth ---
 
-async function getAccessToken() {
-  const jwt = await createJWT();
-  const response = await fetch(SERVICE_ACCOUNT.token_uri, {
+async function getAccessToken(env) {
+  const jwt = await createJWT(env);
+  const response = await fetch(TOKEN_URI, {
     method: 'POST',
     headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
     body: `grant_type=${encodeURIComponent('urn:ietf:params:oauth:grant-type:jwt-bearer')}&assertion=${jwt}`
@@ -199,14 +166,14 @@ async function getAccessToken() {
   return data.access_token;
 }
 
-async function createJWT() {
+async function createJWT(env) {
   const now = Math.floor(Date.now() / 1000);
 
   const header = { alg: 'RS256', typ: 'JWT' };
   const payload = {
-    iss: SERVICE_ACCOUNT.client_email,
+    iss: env.GCP_CLIENT_EMAIL,
     scope: 'https://www.googleapis.com/auth/analytics.readonly',
-    aud: SERVICE_ACCOUNT.token_uri,
+    aud: TOKEN_URI,
     iat: now,
     exp: now + 3600
   };
@@ -215,7 +182,7 @@ async function createJWT() {
   const payloadB64 = b64url(JSON.stringify(payload));
   const signingInput = `${headerB64}.${payloadB64}`;
 
-  const key = await importPrivateKey(SERVICE_ACCOUNT.private_key);
+  const key = await importPrivateKey(env.GCP_PRIVATE_KEY);
   const signature = await crypto.subtle.sign(
     { name: 'RSASSA-PKCS1-v1_5' },
     key,
